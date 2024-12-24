312006c71806f0dded30e85be899c2101081f933
MateoMuscolino <muscolinomateo@gmail.com>
2024-12-24 08:27:59 -0300

    FIX: html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if (inputBox.value === ''){
+function addTask(){ // check if the user just added something
+    if (inputBox.value === ''){ // if the user didn't add something
         alert("Please enter a task!");
     }
-    else {
-        let li = document.createElement("li");
+    else { // if the user just added something
+        let li = document.createElement("li"); // create the li element and append it to the list
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
     }
-    }
+}
