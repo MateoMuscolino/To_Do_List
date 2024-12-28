ed3fa50f5fc71e3056b35954a42084cf7ae3ec0e
MateoMuscolino <muscolinomateo@gmail.com>
2024-12-28 10:07:36 -0300

    ADD: showTask function

--- js/script.js
+++ js/script.js
@@ -14,13 +14,25 @@ function addTask(){ // check if the user just added something
         li.appendChild(span); // append the cross icon to the li element
     }
     inputBox.value = ""; // clear the input box
+    saveData()
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData()
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
