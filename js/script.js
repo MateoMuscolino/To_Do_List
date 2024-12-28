b85914be7f885f4d91aed0bfff875984cec54db2
MateoMuscolino <muscolinomateo@gmail.com>
2024-12-28 10:02:07 -0300

    ADD: check and uncheck the task functionality

--- js/script.js
+++ js/script.js
@@ -15,3 +15,12 @@ function addTask(){ // check if the user just added something
     }
     inputBox.value = ""; // clear the input box
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
